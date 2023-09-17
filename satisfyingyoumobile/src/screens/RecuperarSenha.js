7984b49e5d3a5ad4fa2e4fec3b1bbd24f9c0f0c6
patrikmelod <patrikmelod@gmail.com>
2023-09-17 18:51:56 -0300

    login, nova conta e recuperar senha

---
+++ satisfyingyoumobile/src/screens/RecuperarSenha.js
@@ -0,0 +1,74 @@
+import { View, StyleSheet, Text } from 'react-native'
+import { PaperProvider, MD3LightTheme as DefaultTheme, TextInput, Button } from 'react-native-paper'
+import { useState } from 'react'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: 'blue',
+        secondary: 'green'
+    }
+}
+
+const RecuperarSenha = (props) => {
+
+    const [txtEmail, setEmail] = useState('')
+
+    const recuperar = () => {
+        props.navigation.goBack()
+    }
+    
+    return (
+        <PaperProvider>
+            <View style={estilos.view}>
+                <View style={estilos.top}>
+                    <TextInput
+                        label="Email"
+                        value={txtEmail}
+                        onChangeText={setEmail}
+                        placeholder='Digite seu email'
+                    />
+                    <Text style={estilos.texto}>
+                        E-mail parece ser inválido
+                    </Text>
+                </View>
+
+                <View style={estilos.bottom}>
+                    <Button mode="contained" buttonColor='#228B22' onPress={recuperar}>
+                        Recuperar
+                    </Button>
+                </View>
+            </View>
+        </PaperProvider>
+    )
+}
+
+const estilos = StyleSheet.create({
+    view: {
+        backgroundColor: '#4B0082',
+        padding: 30,
+        flex: 1,
+        flexDIRECTION: 'column',
+        justifyContent: 'flex-start'
+    },
+    texto: {
+        fontSize: 15,
+        color: '#DC143C',
+        fontFamily: 'AveriaLibre-Bold'
+    },
+    top: {
+        paddingVertical: 190,
+        flex: 0.30,
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+    },
+    bottom: {
+        flex: 0.70,
+        flexDirection: 'column',
+        justifyContent: 'center',
+    }
+})
+
+export default RecuperarSenha;
